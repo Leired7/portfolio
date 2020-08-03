f2dee9d1974d459523a156b7606e2b4d1e857a5f
Leired7 <leirediez.7@gmail.com>
2020-08-03 12:48:37 +0200

    [FIX] Modal onClick

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -6,8 +6,8 @@ import Skills from 'components/Skills';
 
 export default function Modal({ onClose }) {
   return (
-    <div className="modal" onClick={onClose}>
-      <div className="modal__content" onClick={onClose}>
+    <div className="modal" >
+      <div className="modal__content" >
         <div className="modal__content-button ">
           <button type="button" className="modal-button" onClick={onClose}>
             Cerrar
